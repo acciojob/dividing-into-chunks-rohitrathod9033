e753720f844756528465b0a12c1cf54edf161127
Rohit Rathod <86867651+rohitrathod9033@users.noreply.github.com>
2025-06-18 08:03:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  const result = [];
+  let current = [];
+  let currentSum = 0;
+
+  for (let num of arr) {
+    if (currentSum + num <= n) {
+      current.push(num);
+      currentSum += num;
+    } else {
+      if (current.length > 0) {
+        result.push(current);
+      }
+      current = [num];
+      currentSum = num;
+    }
+  }
+
+  if (current.length > 0) {
+    result.push(current);
+  }
+
+  return result;
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+console.log(divide(arr, 5));
